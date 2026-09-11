5e8da0bdf2a08391386089004ce1ff4c155c3979
kicikhaluk <f.halukkicik@outlook.com>
2026-09-12 01:25:49 +0300

    add lesson 4 search and bash tools

---
+++ src/04-search-and-bash/workspace/src/index.ts
@@ -0,0 +1,4 @@
+export function add(a: number, b: number): number {
+  // TODO: validate inputs are finite numbers
+  return a + b;
+}
---
+++ src/04-search-and-bash/workspace/src/util.ts
@@ -0,0 +1,5 @@
+export function formatName(first: string, last: string): string {
+  // TODO: handle empty strings
+  // TODO: trim whitespace
+  return `${first} ${last}`;
+}
